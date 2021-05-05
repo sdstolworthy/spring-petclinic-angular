8f2a686d01a27851e334f1b87496e04ac1cbe260
wuxin zeng <wuzeng@redhat.com>
2021-05-05 15:04:03 -0700

    rm direct connect

--- protractor.conf.js
+++ protractor.conf.js
@@ -15,7 +15,7 @@ exports.config = {
   frameworkPath: require.resolve("protractor-cucumber-framework"),
   seleniumAddress:
     process.env.SELENIUM_ADDRESS,
-  directConnect: true,
+  // directConnect: true,
   cucumberOpts: {
     require: "features/step_definitions/*.ts",
     format: "json:.tmp/results.json",
